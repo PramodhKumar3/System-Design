64473c2c8bcbebb7e12d90af6de13c4c9c5702b2
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-09-23 23:06:35 +0530

    Delete Creational/com/creational/singleton directory

--- Creational/com/creational/singleton/NotificationModule.java
+++
@@ -1,9 +0,0 @@
-package com.creational.singleton;
-
-public class NotificationModule {
-	// NotificationModule method trying to create singleton object instance
-	public void processNotification() {
-		Logger logger = Logger.createInstance();
-		logger.log("NotificationModule: Sending Notification....");
-	}
-}
