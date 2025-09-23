8477e9551352650273b88b3d54f6becef6e43ad7
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-09-23 23:07:45 +0530

    Add files via upload
    Uploaded singleton design pattern

---
+++ Creational/com/singleton/UserModule.java
@@ -0,0 +1,9 @@
+package com.creational.singleton;
+
+public class UserModule {
+	// UserModule method trying to create singleton object instance
+	public void processUser() {
+		Logger logger = Logger.createInstance();
+		logger.log("UserModule: Processing User data....");
+	}
+}
