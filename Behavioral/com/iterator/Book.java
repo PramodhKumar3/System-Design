0079d94c8b9869420522cecb88662803304fc106
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-11-08 21:13:38 +0530

    Uploaded Iterator Design Pattern

---
+++ Behavioral/com/iterator/Book.java
@@ -0,0 +1,21 @@
+package com.iterator;
+
+public class Book {
+	private String title;
+	private String author;
+
+	public Book(String title, String author) {
+		super();
+		this.title = title;
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+}
