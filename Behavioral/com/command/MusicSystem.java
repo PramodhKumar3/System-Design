90f9931c49a6dfcce3f2701c072d4043501a1225
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-11-23 12:27:57 +0530

    Uploaded Command Design Pattern

---
+++ Behavioral/com/command/MusicSystem.java
@@ -0,0 +1,12 @@
+package com.command;
+
+public class MusicSystem {
+	public void play() {
+		System.out.println("Music is PLAYING");
+	}
+
+	public void stop() {
+		System.out.println("Music is STOPPED");
+	}
+
+}
