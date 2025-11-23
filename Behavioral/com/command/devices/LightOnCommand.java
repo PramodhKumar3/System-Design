94158665e97bb4d3682c349454d3d997992ce6c6
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-11-23 12:29:22 +0530

    Uploaded Command Design Pattern

---
+++ Behavioral/com/command/devices/LightOnCommand.java
@@ -0,0 +1,19 @@
+package com.command.devices;
+
+import com.command.Light;
+
+public class LightOnCommand implements Command {
+	private Light light;
+
+	public LightOnCommand(Light light) {
+		this.light = light;
+	}
+
+	public void execute() {
+		light.on();
+	}
+
+	public void undo() {
+		light.off();
+	}
+}
