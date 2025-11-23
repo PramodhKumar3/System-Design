2cc340a56aa21af5c193c6173153eda125e1c701
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-11-23 12:26:03 +0530

    Uploaded Command Design Pattern

---
+++ Behavioral/com/command/GarageDoor.java
@@ -0,0 +1,11 @@
+package com.command;
+
+public class GarageDoor {
+	public void open() {
+		System.out.println("Garage Door is OPEN");
+	}
+
+	public void close() {
+		System.out.println("Garage Door is CLOSED");
+	}
+}
