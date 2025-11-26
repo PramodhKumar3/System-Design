4c562ce9f3b9e13de7806ba7c194ece36c1ff8c0
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-11-26 12:00:12 +0530

    Uploaded Memento Design Pattern

---
+++ Behavioral/com/memento/TextEditor.java
@@ -0,0 +1,21 @@
+package com.memento;
+
+public class TextEditor {
+	private String content = "";
+
+	public void type(String words) {
+		content += words;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public Memento save() {
+		return new Memento(content);
+	}
+
+	public void restore(Memento memento) {
+		content = memento.getState();
+	}
+}
