b61541a1e18d513764d96f1835e17461afb549f8
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-12-09 11:39:43 +0530

    Uploaded Adapter Design Pattern

---
+++ Structural/com/adapter/OldPaymentSystem.java
@@ -0,0 +1,9 @@
+package com.adapter;
+
+public class OldPaymentSystem {
+
+	public void makePayment(String amount) {
+		System.out.println("Payment of $" + amount + " processed using OldPaymentSystem.");
+	}
+
+}
