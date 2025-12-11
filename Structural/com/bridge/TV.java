3711d2d56c63af35515ecdd6e321950feebe8966
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-12-11 14:49:02 +0530

    Uploaded Bridge Design Pattern

---
+++ Structural/com/bridge/TV.java
@@ -0,0 +1,15 @@
+package com.bridge;
+
+public class TV implements Device {
+
+	@Override
+	public void turnOn() {
+		System.out.println("TV is now ON.");
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("TV is now OFF.");
+	}
+
+}
