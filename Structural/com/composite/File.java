b380fd2151356773714fe9ec22174d358bd22955
Pramodh Kumar Tamminaina <117188692+PramodhKumar3@users.noreply.github.com>
2025-12-12 19:25:43 +0530

    Uploaded Composite Design Pattern

---
+++ Structural/com/composite/File.java
@@ -0,0 +1,23 @@
+package com.composite;
+
+public class File implements FileSystemComponent {
+	private String name;
+	private int size;
+
+	public File(String name, int size) {
+		super();
+		this.name = name;
+		this.size = size;
+	}
+
+	@Override
+	public void display(String indent) {
+		System.out.println(indent + "- File: " + name + " (" + size + "KB)");
+	}
+
+	@Override
+	public int getSize() {
+		return size;
+	}
+
+}
